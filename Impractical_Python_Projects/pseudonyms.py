97bcb024d20b217799eea3fceae7d83bbfe0d6ed
John LaTorre <johnjlatorre@gmail.com>
2022-03-13 16:49:16 -0400

    More folder changes, intro to a new book

---
+++ Impractical_Python_Projects/pseudonyms.py
@@ -0,0 +1,6 @@
+#Silly name generator from Chapter 1 of Impractical Python Projects by No Starch Press
+
+import sys, random
+
+print("Welcome to the Silly Name Generator.\n")
+print("Just a bunch of silly names that are not related to anything.\n")
---
+++ Self_projects/randomTotalWar.py
@@ -0,0 +1,308 @@
+#Random faction picker for entire Total War series
+import random
+
+shogun = {
+    "campaign":["Grand"],
+    "faction": {
+        "Grand":  ["Hojo", "Imagawa", "Mori", "Oda", "Shimazu", "Takeda", "Uesugi", "Mongols"]
+    },
+    "sub_faction": {"None": [""]}
+}
+
+medieval = {
+    "campaign":["Viking", "Early", "High", "Late"],
+    "faction": {
+        "Viking": ["Irish", "Mercians", "Northumbrians", "Picts", "Saxons", "Scots", "Vikings", "Welsh"],
+        "Early": ["Almohads", "Aragonese", "Byzantines", "Danes", "Egyptians", "English", "French", "Germans",
+         "Hungarians", "Italians", "Polish", "Sicilians", "Spanish", "Turkish"],
+        "High": ["Almohads", "Aragonese", "Byzantines", "Danes", "Egyptians", "English", "French", "Germans",
+         "Hungarians", "Italians", "Polish", "Russians", "Sicilians", "Spanish", "Turkish"],
+        "Late": ["Almohads", "Aragonese", "Byzantines", "Danes", "Egyptians", "English", "French", "Germans",
+         "Hungarians", "Italians", "Polish", "Russians", "Sicilians", "Spanish", "Turkish"]
+         },
+    "sub_faction": {"None": [""]}
+}
+
+rome = {
+    "campaign":["Grand", "Barbarian Invasion", "Alexander"],
+    "faction": {
+        "Grand": ["Julii", "Brutii", "Scipii", "Macedonians", "Egyptians", "Seleucids", "Carthaginians", "Parthians", 
+            "People of Pontus", "Gauls", "Germans", "Britons", "Armenians", "Dacians", "Greeks", "Numidians", "Scythiaans", "Spanish", "Thracians"],
+        "Barbarian Invasion": ["Huns", "Goths", "Sarmatians", "Vandals", "Germanic Frankish Tribes", "Saxons", "Celts", 
+            "Sassanid Empire", "Roxolani", "Ostrogoths", "Romano-British", "Slavs", "Burgundii", "Berbers", "Alemanni", "Eastern Roman Empire", "Western Roman Empire", "Lombardi"],
+        "Alexander": ["Macedon"]
+        },
+    "sub_faction": {"None": [""]}
+}
+
+medieval2 = {
+    "campaign":["Grand", "Americas", "Britannia", "Crusades", "Teutonic"],
+    "faction": {
+        "Grand": ["England", "France", "Holy Roman Empire", "Spain", "Venice"],
+        "Americas": ["New Spain", "Aztec Empire", "Mayans", "Apachean Tribes"],
+        "Britannia": ["England", "Wales", "Scotland", "Ireland", "Norway"],
+        "Crusades": ["Kingdom of Jerusalem", "Principality of Antioch", "Byzantine Empire", "Egypt", "The Turks"],
+        "Teutonic": ["Teutonic Order", "Lithuania", "Denmark", "Novgorod"]
+        },
+    "sub_faction": {"None": [""]}
+}
+
+empire = {
+    "campaign":["Grand", "Warpath"],
+    "faction": {
+        "Grand": ["United States", "Great Britain", "France", "Austria", "United Provinces", "Spain", "Prussia", "Poland-Lithuania", 
+        "Ottoman Empire", "Russia", "Sweden", "Maratha Confederacy"],
+        "Warpath": ["Iroquois Confederacy", "Huron-Wyandot", "Plains Nations", "Pueblo Nations", "Cherokee Nations"]
+        },
+    "sub_faction": {"None": [""]}
+}
+
+napoleon = {
+    "campaign":["Grand", "Peninsular"],
+    "faction": {
+        "Grand": ["Austria", "Great Britain", "Prussia", "Russia", "Napoleon"],
+        "Peninsular": ["Spain", "Great Britain", "France"]
+        },
+    "sub_faction": {"None": [""]}
+}
+
+shogun2 = {
+    "campaign":["Grand", "Rise", "Fall"],
+    "faction": {
+        "Grand": ["Chosokabe", "Shimazu", "Hojo", "Mori", "Oda", "Date", "Takeda", "Tokugawa", "Uesugi", "Hattori", "Ikko Ikki", "Otomo"],
+        "Rise": ["Kamakura Minamoto", "Kiso Minamoto", "Yashima Taira", "Fukuhara Taira", "Kubota Fujiwara", "Hiraizumi Fujiwara"],
+        "Fall": ["Shogunate", "Imperial"]
+        },
+    "sub_faction": {
+        "Fall": {
+            "Shogunate": ["Obama", "Sendai", "Jozai", "Aizu", "Nagaoka"],
+            "Imperial": ["Satsuma", "Tosa", "Choshu", "Tsu", "Saga"]
+            }
+    }
+}
+
+rome2 = {
+    "campaign":["Grand", "Augustus", "Empire Divided", "Rise of the Republic", "Ceasar in Gaul", "Hannibal", "Wrath of Sparta"],
+    "faction": {
+        "Grand": ["Rome", "Carthage", "Balkan Tribes", "Gallic Tribes", "Successor Kingdoms", "Desert Kingdoms", "Greek States", "Iberian Tribes", 
+            "Nomadic Tribes", "Eastern Empires", "Black Sea Colonies", "Britannic Tribes", "Germanic Tribes"],
+        "Augustus": ["Rome", "Eastern Empires", "Balkan Tribes", "Successor Kingdoms", "Britannic Tribes", "Germanic Tribes"],
+        "Empire Divided": ["Rome", "Germanic Kingdoms", "Eastern Empires", "Nomadic Tribes", "Britannic Celts"],
+        "Rise of the Republic": ["Greek States", "Gallic Tribes", "Italian Civs", "Italic Tribes", "Nuragic Tribes"],
+        "Ceasar in Gaul": ["Rome", "Arverni", "Suebi", "Nervii"],
+        "Hannibal": ["Carthage", "Rome", "Syracuse", "Lusitani", "Arevaci"],
+        "Wrath of Sparta": ["Athenai", "Boiotian League", "Korinthos", "Sparta"]
+        },
+    "sub_faction": {
+        "Grand":
+        {
+            "Rome": ["Julia", "Junia", "Cornelia"],
+            "Carthage": ["Magonid", "Hannonid", "Barcid"], 
+            "Balkan Tribes": ["Odrysian Kingdom", "Getae", "Ardiaei", "Tylis"], 
+            "Gallic Tribes": ["Boii", "Arverni", "Nervii", "Galatia"], 
+            "Successor Kingdoms": ["Seleucid", "Egypt", "Macedon", "Baktria"], 
+            "Desert Kingdoms": ["Masaesyli", "Nabataea", "Saba", "Kush"], 
+            "Greek States": ["Massilia", "Sparta", "Athens", "Epirus", "Syracuse"], 
+            "Iberian Tribes": ["Lusitani", "Arevaci"], 
+            "Nomadic Tribes": ["Roxolani", "Massagetae", "Royal Scythia"], 
+            "Eastern Empires": ["Parthia", "Armenia", "Pontus"], 
+            "Black Sea Colonies": ["Pergamon", "Cimmeria", "Colchis"], 
+            "Britannic Tribes": ["Iceni"], 
+            "Germanic Tribes": ["Suebi"]
+        },
+        "Augustus":
+        {
+            "Rome": ["Lepidus", "Octavian", "Antony", "Pompey"],
+            "Eastern Empires": ["Parthia", "Armenia"], 
+            "Balkan Tribes": ["Dacia"],  
+            "Successor Kingdoms": ["Egypt"],  
+            "Britannic Tribes": ["Iceni"], 
+            "Germanic Tribes": ["Marcomanni"]
+        },
+        "Empire Divided":
+        {
+            "Rome": ["Palmyra", "Gallic Rome", "Rome"],
+            "Germanic Kingdoms": ["Saxoni", "Gothi", "Marcomanni"],
+            "Eastern Empires": ["Sassanids", "Armenia"],   
+            "Nomadic Tribes": ["Alani"],  
+            "Britannic Celts": ["Caledonii"]
+        },
+        "Rise of the Republic":
+        {
+            "Greek States": ["Syracuse", "Taras"],
+            "Gallic Tribes": ["Senones", "Insubres"],
+            "Italian Civs": ["Rome", "Tarchuna"],   
+            "Italic Tribes": ["Samnites", "Veneti"],  
+            "Nuragic Tribes": ["Jolei"]
+        },
+    }
+}
+
+attila = {
+    "campaign":["Grand", "Last Roman", "Charlemagne"],
+    "faction": {
+        "Grand": ["Nomadic Tribes", "Roman Empire", "Eastern Empires", "Great Migrators", "Barbarian Kingdoms", "Norsemen", "Celts", "Desert Kingdoms", "Slavs"],
+        "Last Roman": ["Roman Expedition", "Vandalic Kingdom", "Ostrogothic Kingdom", "Frankish Kingdom", "Visigothic Kingdom"],
+        "Charlemagne": ["Charlemagne", "Asturias", "Avars", "Cordoba", "Danes", "Lombards", "Mercia", "Westphalia"]
+        },
+    "sub_faction": {
+        "Grand":
+        {
+            "Nomadic Tribes": ["Huns", "White Huns"],
+            "Roman Empire": ["Eastern", "Western"], 
+            "Eastern Empires": ["Sassanid"], 
+            "Great Migrators": ["Suebians", "Vandals", "Alans", "Ostrogoths", "Visigoths"], 
+            "Barbarian Kingdoms": ["Langobards", "Burgundians", "Saxons", "Alamans", "Franks"], 
+            "Norsemen": ["Danes", "Jutes", "Geats"], 
+            "Celts": ["Ebdanians", "Picts", "Caledonians"], 
+            "Desert Kingdoms": ["Aksum", "Lakhmids", "Tanukhids", "Himyar", "Garamantians"], 
+            "Slavs": ["Sclavenians", "Anteans", "Venedians"]
+        }
+    }
+}
+
+thrones = {
+    "campaign":["Grand"],
+    "faction": {
+        "Grand": ["English", "Welsh", "Gaelic", "Great Viking Army", "Viking Sea Kings"]
+        },
+    "sub_faction": {
+        "Grand":
+        {
+            "English": ["Mierce", "West Seaxe"],
+            "Welsh": ["Gwined", "Strat Clut"], 
+            "Gaelic": ["Circenn", "Mide"], 
+            "Great Viking Army": ["East Engle", "Northymbre"], 
+            "Viking Sea Kings": ["Dyflin", "Sudreyar"]
+        }
+    }
+}
+
+troy = {
+    "campaign":["Historical", "True Myth", "Full Myth"],
+    "faction": {
+        "Historical": ["Danaans", "Trojans", "Amazons"],
+        "True Myth": ["Danaans", "Trojans", "Amazons"],
+        "Full Myth": ["Danaans", "Trojans", "Amazons"]
+        },
+    "sub_faction": {
+        "Historical":
+        {
+            "Danaans": ["Achilles", "Odysseus", "Agamemnon", "Menelaus", "Ajax", "Diomedes"],
+            "Trojans": ["Hector", "Paris", "Aeneas", "Sarpedon", "Rhesus", "Memnon"], 
+            "Amazons": ["Hippolyta", "Penthesilea"]
+        },
+        "True Myth":
+        {
+            "Danaans": ["Achilles", "Odysseus", "Agamemnon", "Menelaus", "Ajax", "Diomedes"],
+            "Trojans": ["Hector", "Paris", "Aeneas", "Sarpedon", "Rhesus", "Memnon"], 
+            "Amazons": ["Hippolyta", "Penthesilea"]
+        },
+        "Full Myth":
+        {
+            "Danaans": ["Achilles", "Odysseus", "Agamemnon", "Menelaus", "Ajax", "Diomedes"],
+            "Trojans": ["Hector", "Paris", "Aeneas", "Sarpedon", "Rhesus", "Memnon"], 
+            "Amazons": ["Hippolyta", "Penthesilea"]
+        }
+    }
+}
+
+threeKingdoms = {
+    "campaign":["Eight Princes", "Fates Divided", "World Betrayed", "Rise of Warlords", "Mandate of Heaven"],
+    "faction": {
+        "Eight Princes": ["Sima Yong", "Sima Liang", "Sima Jiong", "Sima Ai", "Sima Lun", "Sima Ying", "Sima Wei", "Sima Yue"],
+        "Fates Divided": ["Warlords", "Regional Lords", "Bandits", "Southern Tribes", "Yellow Turban"],
+        "World Betrayed": ["Warlords", "Regional Lords", "Bandits", "Southern Tribes", "Yellow Turban"],
+        "Rise of Warlords": ["Coalition", "Empire", "Bandits", "Southern Tribes", "Yellow Turban"],
+        "Mandate of Heaven": ["Imperial Warlords", "Imperial Governors", "Han Dynasty", "Yellow Turban"]
+        },
+    "sub_faction": {
+        "Fates Divided":
+        {
+            "Warlords": ["Yuan Shao", "Sun Ce", "Cao Cao", "Liu Bei"],
+            "Regional Lords": ["Liu Zhang", "Liu Biao", "Ma Teng", "Shi Xie"], 
+            "Bandits": ["Zheng Jiang", "Zhang Yan"], 
+            "Southern Tribes": ["Shamoke", "Mulu", "Meng Huo"], 
+            "Yellow Turban": ["Gong Du"]
+        },
+        "World Betrayed":
+        {
+            "Warlords": ["Yuan Shao", "Sun Ce", "Cao Cao", "Liu Bei", "Lu Bu", "Gongsun Zan", "Yan Shu", "Shi Xie"],
+            "Regional Lords": ["Liu Zhang", "Liu Biao", "Ma Teng", "Kong Rong", "Liu Chong"], 
+            "Bandits": ["Zheng Jiang", "Zhang Yan", "Yan Baihu"], 
+            "Southern Tribes": ["Shamoke", "Mulu", "Meng Huo", "Lady Zhurong"], 
+            "Yellow Turban": ["He Yi"]
+        },
+        "Rise of Warlords":
+        {
+            "Coalition": ["Cao Cao", "Yuan Shao", "Kong Rong", "Tao Qian", "Yuan Shu", "Liu Bei", "Gongsun Zan", "Sun Jian"],
+            "Empire": ["Liu Biao", "Ma Teng", "Liu Chong", "Liu Yan", "Shi Xie"], 
+            "Bandits": ["Zheng Jiang", "Zhang Yan", "Yan Baihu"], 
+            "Southern Tribes": ["Shamoke", "Mulu", "Meng Huo", "Lady Zhurong"], 
+            "Yellow Turban": ["He Yi", "Huang Shao", "Gong Du"]
+        },
+        "Mandate of Heaven":
+        {
+            "Imperial Warlords": ["Sun Jian", "Cao Cao"],
+            "Imperial Governors": ["Yuan Shu", "Tao Qian", "Lu Zhi", "Liu Biao", "Ma Teng"], 
+            "Han Dynasty": ["Liu Bei", "Liu Chong", "Liu Hong"], 
+            "Yellow Turban": ["Zhang Jue", "Zhang Liang", "Zhang Bao"]
+        },
+    }
+}
+
+warhammer = {
+    "campaign":["Mortal Empires"],
+    "faction": {
+        "Mortal Empires": ["High Elves", "Lizardmen", "Dark Elves", "Skaven", "Vampire Coast", "Tomb Kings", "The Empire", "Dwarfs", "Greenskins", "Vampire Counts",
+            "Norsca", "Bordeleaux", "Wood Elves", "Beastmen", "Chaos"]
+        },
+    "sub_faction": {
+        "Mortal Empires":
+        {
+            "High Elves": ["Eataine", "Order of Loremasters", "Avelorn", "Nagarythe", "Yvresse", "Knights of Caledor"],
+            "Lizardmen": ["Hexoatl", "Last Defenders", "Cult of Sotek", "Tlaqua", "Spirit of the Jungle", "Itza", "Ghosts of Pahuax"],
+            "Dark Elves": ["Naggarond", "Cult of Pleasure", "Har Ganeth", "The Blessed Dread", "Hag Graef", "The Thousand Maws"],
+            "Skaven": ["Mors", "Pestilens", "Rictus", "Skryre", "Eshin", "Moulder"],
+            "Vampire Coast": ["The Awakened", "The Dreadfleet", "Pirates of Sartosa", "The Drowned"],
+            "Tomb Kings": ["Khemri", "Exiles of Nehek", "Court of Lybaras", "Followers of Nagash"],
+            "The Empire": ["Reikland (Karl Franz)", "Reikland (Volkmar)", "The Golden Order", "Huntmarshal"],
+            "Dwarfs": ["Karaza Karak (Thorgrim)", "Karaza Karak (Grombrindal)", "Clan Angrund", "Karak Kadrin", "Ironbrow"],
+            "Greenskins": ["Grimgor's Ardboyz", "Crooked Moon", "Bloody Handz", "Broken Axe", "Bonerattlaz"],
+            "Vampire Counts": ["Sylvania (Mannfred)", "Sylvania (Helman Ghorst)", "Von Carstein (Vlad)", "Von Carstein (Isabella)", "The Barrow Legion"],
+            "Norsca": ["World Walkers", "Wintertooth"],
+            "Bordeleaux": ["Couronne", "Bordeleaux", "Carcassonne", "Chevaliers de Lyonesse"],
+            "Wood Elves": ["Talsyn", "Argwylon", "Heralds of Ariel", "Wargrove of Woe"],
+            "Beastmen": ["One-eye", "Harbinger of Disaster", "Shadowgave", "Slaughterhorn"], 
+            "Chaos": ["Archaon the Everchosen", "Kholek Suneater", "Prince Sigvald"]
+        }
+    }
+}
+
+games = {
+    "Shogun" : shogun, "Medieval": medieval, "Rome: Remastered": rome, "Medieval II": medieval2, "Empire": empire, "Napoleon": napoleon, "Shogun II": shogun2, "Rome II": rome2,
+        "Attila": attila, "Thrones of Britannia": thrones, "Troy": troy, "Three Kingdoms": threeKingdoms, "Warhammer II": warhammer
+}
+
+def factionPicker():
+    gameName = random.choice(list(games))
+
+    gameDict = games[gameName]
+
+    gameCampaign = random.choice(gameDict["campaign"])
+
+    gameFactionList = gameDict["faction"][gameCampaign]
+
+    gameFaction = random.choice(gameFactionList)
+
+    gameSubFactionDict = gameDict["sub_faction"]
+
+    print("Game: Total War: " + gameName)
+    print("Campaign: " + gameCampaign)
+    print("Faction: " + gameFaction)
+    if gameCampaign in gameSubFactionDict:
+        gameSubFactionList =gameSubFactionDict[gameCampaign][gameFaction]
+        gameSubFaction = random.choice(gameSubFactionList)
+        print("Sub Faction: " + gameSubFaction)
+
+factionPicker()
