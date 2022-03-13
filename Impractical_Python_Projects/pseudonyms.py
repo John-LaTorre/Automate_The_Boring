40696aa5f42aaaa6eef6277922a6e08f3f2d1037
John LaTorre <johnjlatorre@gmail.com>
2022-03-13 17:21:05 -0400

    sill name generator

--- Impractical_Python_Projects/pseudonyms.py
+++ Impractical_Python_Projects/pseudonyms.py
@@ -1,6 +1,33 @@
-#Silly name generator from Chapter 1 of Impractical Python Projects by No Starch Press
+"""Silly name generator from Chapter 1 of Impractical Python Projects by No Starch Press"""
 
-import sys, random
+import sys
+import random
 
-print("Welcome to the Silly Name Generator.\n")
-print("Just a bunch of silly names that are not related to anything.\n")
+def main():
+    print("Welcome to the Silly Name Generator.\n")
+    print("Just a bunch of silly names that are not related to anything.\n")
+
+    first = ('Baby Oil', 'Buttocks', 'Chad', 'Cleet', 'Cornbread', 'Dark Skies',
+    'Elphonso', 'Fancypants', 'Greasy Jim', 'Jimbo', 'Scratchensniff', 'Snoobs',
+    'Sweet Tea', 'Wheezy Joe')
+
+    last = ('Appleyard', 'Bloominshine', 'Clutterbuck', 'Cocktoasten', 'Goodpasture',
+    'Hooperbag', 'Jefferson', 'Jenkins', 'Pinkerton', 'Quakenbush', 'Snuggleshine',
+    'Wallbanger', 'Wigglesworth')
+
+    while True:
+        first_name = random.choice(first)
+
+        last_name = random.choice(last)
+
+        print("\n\n")
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
+
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+        if try_again.lower() == "n":
+            break
+    input("\nPress Enter to exit.")
+
+if __name__ == "__main__":
+    main()
