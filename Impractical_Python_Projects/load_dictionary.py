704e7d61fc7c85749b96d6b84f6d8f4d66e72527
John LaTorre <johnjlatorre@gmail.com>
2023-06-08 17:10:24 -0400

    Prior to TED, 30minute exercise

--- Impractical_Python_Projects/load_dictionary.py
+++ Impractical_Python_Projects/load_dictionary.py
@@ -1,13 +1,13 @@
 import sys
+import os
 
 def load(file):
     """Open a text file & return a list of lowercase strings"""
     try:
-        with open(file) as in_file:
+        with open(os.path.join(sys.path[0], file)) as in_file:
             loaded_txt = in_file.read().strip().split('\n')
             loaded_txt = [x.lower() for x in loaded_txt]
             return loaded_txt
     except IOError as e:
         print("{}\nError opening {}. Terminating program.".format(e, file), file = sys.stderr)
         sys.exit
-        
